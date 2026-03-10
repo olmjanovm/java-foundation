516bef0969c186e98a759bd6cbe3edbfe4e9e1a2
olimjanovm <olmjanovm@gmail.com>
2026-03-10 15:36:40 +0500

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,129 +1,58 @@
-
-public  class Main {
-    public static void main(String[] args){
-
-//    begin 1
-
-//        int a = 5;
-//        float p = 4*a;
-//        System.out.println("Kvadratning perimetri: "+ p);
 //
-//        begin 2
+//public  class Main {
+//    public static void main(String[] args){
 //
-//        int a = 5;
-//        float s = a*a;
-//        System.out.println("Kvadratinig yuzasi; "+ s);
-
-//     begin 3
-//        int a = 5;
-//        int  b= 10;
+////    begin 1
 //
-//        float s = a*b;
-//        float p = 2*(a+b);
+////        int a = 5;
+////        float p = 4*a;
+////        System.out.println("Kvadratning perimetri: "+ p);
+////
+////        begin 2
+////
+////        int a = 5;
+////        float s = a*a;
+////        System.out.println("Kvadratinig yuzasi; "+ s);
 //
-//        System.out.println("Yuzasi: "+ s);
-//        System.out.println("Perimetri: "+ p);
-
-//    begin 4
-
-//        int d = 10;
-//        float pi = 3.14f;
-//        float l = pi * d;
-//        System.out.println("Aylananing Uzunligi: "+ l);
-
-//        begin 5
-
-//        int a = 18;
-//        int v = a*a*a;
-//        int s = 6*(a*a);
+////     begin 3
+////        int a = 5;
+////        int  b= 10;
+////
+////        float s = a*b;
+////        float p = 2*(a+b);
+////
+////        System.out.println("Yuzasi: "+ s);
+////        System.out.println("Perimetri: "+ p);
 //
-//        System.out.println("Hajmi :" + v + " To'la sitri: " + s);
-
-
-
-        int[] numbers = {14, 7, 19, 3, 6, 10};
-
-
-        for (int i = 0; i< numbers.length; i++){
-            System.out.println(numbers[i]);
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+////    begin 4
+//
+////        int d = 10;
+////        float pi = 3.14f;
+////        float l = pi * d;
+////        System.out.println("Aylananing Uzunligi: "+ l);
+//
+////        begin 5
+//
+////        int a = 18;
+////        int v = a*a*a;
+////        int s = 6*(a*a);
+////
+////        System.out.println("Hajmi :" + v + " To'la sitri: " + s);
+//
+//
+//
+//        int[] numbers = {14, 7, 19, 3, 6, 10};
+//
+//
+//        for (int i = 0; i< numbers.length; i++){
+//            System.out.println(numbers[i]);
+//
+//        }
+//
+
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Salom GitHub axli");
     }
 }
-
