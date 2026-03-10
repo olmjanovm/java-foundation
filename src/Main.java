0f2776f6a686eb83072a4dd2f6e87ad7704751c9
olimjanovm <olmjanovm@gmail.com>
2026-03-10 15:17:57 +0500

    Java loyihamning birinchi versiyasi

---
+++ src/Main.java
@@ -0,0 +1,136 @@
+
+public  class Main {
+    public static void main(String[] args){
+
+//    begin 1
+
+//        int a = 5;
+//        float p = 4*a;
+//        System.out.println("Kvadratning perimetri: "+ p);
+//
+//        begin 2
+//
+//        int a = 5;
+//        float s = a*a;
+//        System.out.println("Kvadratinig yuzasi; "+ s);
+
+//     begin 3
+//        int a = 5;
+//        int  b= 10;
+//
+//        float s = a*b;
+//        float p = 2*(a+b);
+//
+//        System.out.println("Yuzasi: "+ s);
+//        System.out.println("Perimetri: "+ p);
+
+//    begin 4
+
+//        int d = 10;
+//        float pi = 3.14f;
+//        float l = pi * d;
+//        System.out.println("Aylananing Uzunligi: "+ l);
+
+//        begin 5
+
+//        int a = 18;
+//        int v = a*a*a;
+//        int s = 6*(a*a);
+//
+//        System.out.println("Hajmi :" + v + " To'la sitri: " + s);
+
+
+
+        int[] numbers = {14, 7, 19, 3, 6, 10};
+
+
+//        for (int i = 0; i< numbers.length; i++){
+//            System.out.println(numbers[i]);
+//
+//        }
+
+        int  i = 0;
+
+        while (i < numbers.length){
+            System.out.println(numbers[i]);
+            i++;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
